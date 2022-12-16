05a85b0ddedb6aa16e92ba846065c75b86cf3de9
jhonatta costa <jhonattan.ramon.17@gmail.com>
2022-12-16 12:40:41 -0300

    dae
    aad

--- PEDRI/JORNAL-DO-CLIMA(PEDRO)/script.js
+++ PEDRI/JORNAL-DO-CLIMA(PEDRO)/script.js
@@ -1,36 +1,87 @@
 async function getTempo() {
-    const uri = 'https://api.hgbrasil.com/weather?key=c1bda15e&format=json-cors&city_name=Rio Largo,AL';
-    const encodedURI = encodeURI(uri);
-    
-    const resposta = await fetch(encodedURI);
-    const json = await resposta.json();
-    const results = json.results
-    console.log(results);
+  const uri = 'https://api.hgbrasil.com/weather?key=c1bda15e&format=json-cors&city_name=Rio Largo,AL';
+  const encodedURI = encodeURI(uri);
 
-    const apiHora = document.querySelector('.api-hora')
-    const apiData = document.querySelector('.api-data')
-    const apiDia = document.querySelector('.api-dia')
-    const apiCidade = document.querySelector('.api-cidade')
-    const apiTemperatura= document.querySelector('.api-temperatura')
-    const divPersonagem = document.querySelector('#div-img')
-    console.log(divPersonagem);
+  const resposta = await fetch(encodedURI);
+  const json = await resposta.json();
+  const results = json.results
+  console.log(results);
+  
+ let arrayImg = ['img/kokushibouu.svg', 'img/Rengokuu.svg', 'img/Tanjiroo.svg','img/Tokitoo.svg','img/Zenitsuu.svg']
+  
+let container = document.querySelector('.container')
+const criarTela = ()=>{
+    
+    
+    for( let i = 0; i <= 7; i++){
+      
+      container.innerHTML += `
+        <div class="fixeds ${results.forecast[i].condition}">
+
+    <div id="div-img" class="div-img">
+      <img  class="img" src="${arrayImg[i]}" alt="">
+    </div>
+
+     <div class="hora" > <p class="api-hora"> ${results.time}</p> </div>
+
+
+
+    <div class="div-conteudo">
+
+
+      <div class="alinhamento-especifico  div-alinhamento">
+
+        
+        <div class="div-left">
+          <div class="retangulo"> <img src="img/Rectangle 5.svg" alt=""></div>
+         
+
+          <div class="data" > <p class="api-data">${results.forecast[i].date}</p></div>
+          <div class="dia" > <p class="api-dia">${results.forecast[i].weekday}</p> </div>
+
+          <span class="spanCidade">
+            <img class="marcador" src="img/icons8-marcador-50.png" alt="">
+            <span class="cidade">  <p  class="api-cidade"> ${results.city}</p></span>
+          </span>
+                      <span> <p>${results.forecast[i].description} </p> </span>
+
+        </div>
+
+
+        <div class="div-temperatura">
+          
+          <span class="line"><img src="img/Line 1.svg" alt=""></span>
+
+          <span  class="temperatura"> <p class="api-temperatura" >Max:${results.forecast[i].max} °</p>
+          <p class='api-temperatura'> 
+            Min:${results.forecast[i].min} °
+          </p>
+          </span>
+
+          
+          <span hidden
+          class="span-icon-sol"> <img src="img/icon-clima/icon-sol.svg" alt=""></span>
+
+        </div>
+      </div>
+
+    </div>
+
+  
+
+  </div>
+      
+      `
+    }
+    
+    
+  }
+  
+  criarTela()
+  
  
 
-    const carregarInformacoesApi = () => {
-      apiHora.innerHTML = results.time;
-      apiData.innerHTML = results.date;
-      apiDia.innerHTML = results.forecast[0].weekday
-      apiCidade.innerHTML = results.city;
-      apiTemperatura.innerHTML = results.temp
-    }
-
-    const trocaDePersonagem = () => {
-      
-      divPersonagem.innerHTML = 
-    }
-     
-     console.log(json);
-     carregarInformacoesApi()
+  
 }
 
 getTempo();
